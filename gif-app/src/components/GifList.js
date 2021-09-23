945b0ff39f0ee49d6e59edea08c875a8df0153a1
seblopezp <seblopezp@gmail.com>
2021-09-23 08:42:56 -0300

    UPDATE: se cambia por hook

--- gif-app/src/components/GifList.js
+++ gif-app/src/components/GifList.js
@@ -1,35 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { GridListItem } from "./GridListItem";
-
+import React from "react";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GridListItem } from "../components/GridListItem";
 export const GifList = ({ category }) => {
-  const [images, setImages] = useState([]);
-  useEffect(() => {
-    getGifs();
-  }, []);
-
-  const getGifs = async () => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
-      category
-    )}&limit=10&api_key=SDeQeaPOdOnwKdDpTgEPYCzBE99meXqa`;
-
-    const resp = await fetch(url);
-
-    const { data } = await resp.json();
-
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
-
-    setImages(gifs);
-  };
+  const { data: images, loading } = useFetchGifs(category);
 
   return (
     <>
-      <h3>{category}</h3>
+      <h3 className="animate__animated animate__fadeIn">{category}</h3>
+      {loading && (
+        <p className="animate__animated animate__pulse">Loading...</p>
+      )}
       <div className="card-grid">
         {images.map((img) => (
           <GridListItem key={img.id} {...img} />
