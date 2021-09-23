c54a79010d7885dda2e49c4808cb26ca0aef0731
seblopezp <seblopezp@gmail.com>
2021-09-23 08:48:34 -0300

    ADD: se agrega e importa bulma css

--- gif-app/src/index.js
+++ gif-app/src/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { GifApp } from './GifApp';
-import './index.css';
-ReactDOM.render(
-  
-  <GifApp />,
-  document.getElementById('root')
-);
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { GifApp } from "./GifApp";
+import "bulma/css/bulma.min.css";
+import "./index.css";
+ReactDOM.render(<GifApp />, document.getElementById("root"));
