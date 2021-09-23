57b682483b5a081f73a1008e11515fb7a408a718
seblopezp <seblopezp@gmail.com>
2021-09-23 09:40:23 -0300

    UPDATE: se actualiza parrafo

--- gif-app/src/components/GridListItem.js
+++ gif-app/src/components/GridListItem.js
@@ -4,7 +4,7 @@ export const GridListItem = ({ id, title, url }) => {
   return (
     <div className="card animate__animated animate__fadeIn">
       <img src={url} alt={title} />
-      <p>{title}</p>
+      <p className="subtitle is-6">{title}</p>
     </div>
   );
 };
