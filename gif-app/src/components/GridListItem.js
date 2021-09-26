d831637c8fb12cef9311d68f943d17135046a659
seblopezp <seblopezp@gmail.com>
2021-09-26 00:15:18 -0300

    UPDATE: Se agregan proptypes

--- gif-app/src/components/GridListItem.js
+++ gif-app/src/components/GridListItem.js
@@ -1,10 +1,14 @@
 import React from "react";
-
-export const GridListItem = ({ id, title, url }) => {
+import PropTypes from "prop-types";
+export const GridListItem = ({ title, url }) => {
   return (
     <div className="card animate__animated animate__fadeIn">
       <img src={url} alt={title} />
       <p className="subtitle is-6">{title}</p>
     </div>
   );
+};
+GridListItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
