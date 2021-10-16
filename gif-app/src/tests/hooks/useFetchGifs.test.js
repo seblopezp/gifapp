f2b2f8078ef68f71eb9d3d13877c2c16ab3e7122
seblopezp <seblopezp@gmail.com>
2021-10-16 10:11:16 -0300

    UPDATE: se agregan test de hook

--- gif-app/src/tests/hooks/useFetchGifs.test.js
+++ gif-app/src/tests/hooks/useFetchGifs.test.js
@@ -1,8 +1,24 @@
 import { useFetchGifs } from "../../hooks/useFetchGifs";
+import { renderHook } from "@testing-library/react-hooks";
 
 describe("Pruebas en el hook useFetchGifs", () => {
-  test("debe de retornar el estado inicial", () => {
-    const { data, loading } = useFetchGifs("Hunter x hunter");
-    console.log(data, loading);
+  test("debe de retornar el estado inicial", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGifs("Studio Ghibli")
+    );
+    const { data, loading } = result.current;
+    await waitForNextUpdate();
+    expect(data).toEqual([]);
+    expect(loading).toBe(true);
+  });
+
+  test("Debe retornar un arreglo de imagenes y el loading en false", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetchGifs("Studio Ghibli")
+    );
+    await waitForNextUpdate();
+    const { data, loading } = result.current;
+    expect(data.length).toBe(10);
+    expect(loading).toBe(false);
   });
 });
